09f6ccb7573e78015c561ccb1ed1b374bcdfaf82
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-29 14:40:22 -0300

    iniciando body e caixa de pesquisa

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plantApp/pages/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(){
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: (){}
       ),
     );
   }

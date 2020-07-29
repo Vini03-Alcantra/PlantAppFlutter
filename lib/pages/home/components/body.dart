390f0db2b3c05db67fa8d5705277ad249c79d446
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-29 14:56:34 -0300

    ajeitando posição da logo

--- lib/pages/home/components/body.dart
+++ lib/pages/home/components/body.dart
@@ -13,6 +13,11 @@ class Body extends StatelessWidget {
           child: Stack(
             children: <Widget>[
               Container(  
+                padding: EdgeInsets.only(
+                  left: kDefaultPadding,
+                  right: kDefaultPadding,
+                  bottom: 36 + kDefaultPadding
+                ),
                 height: size.height * 0.2 -27,
                 decoration: BoxDecoration(  
                   color: kPrimaryColor,
@@ -20,6 +25,20 @@ class Body extends StatelessWidget {
                     bottomLeft: Radius.circular(36),
                     bottomRight: Radius.circular(36)
                   )
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Text(  
+                      "Hi World",
+                      style: Theme.of(context).textTheme.headline5.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Spacer(),
+                    SizedBox(height: 7),
+                    Image.asset("assets/images/logo.png")
+                  ],
                 )
               ),
               Positioned(  

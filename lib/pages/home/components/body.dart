67eba059c6c8173b1f74796067e58488bd97ab1a
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-30 12:45:53 -0300

    exibindo lista de itens horizontal

--- lib/pages/home/components/body.dart
+++ lib/pages/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantApp/constants.dart';
 import 'package:plantApp/pages/home/components/header_with_search_box.dart';
 import 'package:plantApp/pages/home/components/title_with_more_btn.dart';
 
@@ -7,14 +8,115 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-          child: Column(
+      child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(
             title: "Recomendado",
             press: (){},
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+              RecomendPlantCard(
+                image: "assets/images/image_1.png",
+                title: "Samantha",
+                country: "Rússia",
+                price: 440,
+                press: (){},
+              ),
+              RecomendPlantCard(
+                image: "assets/images/image_1.png",
+                title: "Samantha",
+                country: "Rússia",
+                price: 440,
+                press: (){},
+              ),
+              RecomendPlantCard(
+                image: "assets/images/image_1.png",
+                title: "Samantha",
+                country: "Rússia",
+                price: 440,
+                press: (){},
+              ),
+              ]
+            ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class RecomendPlantCard extends StatelessWidget {
+  const RecomendPlantCard({Key key, this.image, this.title, this.country, this.price, this.press}) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container( 
+      margin: EdgeInsets.only(  
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(  
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10)
+                ),
+                boxShadow: [
+                  BoxShadow(                        
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23)
+                    )
+                  ]                    
+              ),
+              child: Row(
+                children: <Widget>[
+                  RichText( 
+                    text: TextSpan(  
+                      children:[
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: "$country".toUpperCase(),
+                          style: TextStyle(  
+                            color: kPrimaryColor.withOpacity(0.5),
+                          )
+                        )
+                      ]
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\$$price',
+                    style: Theme.of(context)
+                      .textTheme
+                      .button
+                      .copyWith(color: kPrimaryColor) 
+                  )
+                ]
+              )
+            ),
+          ) ]
       ),
     );
   }

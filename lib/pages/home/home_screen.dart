1a0a1953d2a1a43346810acce43931a7c69886c1
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-08-07 14:02:16 -0300

    Criando widget bottom navigation bar

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:plantApp/components/my_bottom_nav_bar.dart';
 import 'package:plantApp/constants.dart';
 import 'package:plantApp/pages/home/components/body.dart';
 
@@ -9,41 +10,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.only(
-          left: kDefaultPadding * 2,
-          right: kDefaultPadding * 2,
-          bottom: kDefaultPadding
-        ),
-        height: 80,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(  
-              offset: Offset(0, -10),
-              blurRadius: 35,
-              color: kPrimaryColor.withOpacity(0.30)
-            )
-          ]
-        ),
-        child: Row(  
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(  
-              icon: SvgPicture.asset("assets/icons/flower.svg"),
-              onPressed: (){}
-            ),
-            IconButton(  
-              icon: SvgPicture.asset("assets/icons/heart-icon.svg"),
-              onPressed: (){}
-            ),
-            IconButton(  
-              icon: SvgPicture.asset("assets/icons/user-icon.svg"),
-              onPressed: (){}
-            )
-          ],
-        )
-      ),
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 

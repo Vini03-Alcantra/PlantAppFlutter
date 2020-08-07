41b86e2d947ff35a2773baaa273a3d29e4a86186
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-08-07 13:24:58 -0300

    criando page featured_plants.dart

--- lib/pages/home/components/body.dart
+++ lib/pages/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plantApp/constants.dart';
+import 'package:plantApp/pages/home/components/featured_plants.dart';
 import 'package:plantApp/pages/home/components/header_with_search_box.dart';
 import 'package:plantApp/pages/home/components/recomneds_plants.dart';
 import 'package:plantApp/pages/home/components/title_with_more_btn.dart';
@@ -15,60 +16,12 @@ class Body extends StatelessWidget {
           TitleWithMoreBtn(title: "Recomendado",press: (){}),
           RecomendsPlants(),
           TitleWithMoreBtn(title: "Futuras Plantas",press: (){}),
-          FeaturedPlants()
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding)
         ],
       ),
     );
   }
 }
 
-class FeaturedPlants extends StatelessWidget {
-  const FeaturedPlants({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: <Widget>[
-          FeaturePlantCard(image: "assets/images/bottom_img_1.png", press: (){}),
-          FeaturePlantCard(image: "assets/images/bottom_img_2.png", press: (){})
-        ]            
-      ),
-    );
-  }
-}
-
-class FeaturePlantCard extends StatelessWidget {
-  const FeaturePlantCard({Key key, this.image, this.press,}) : super(key: key);
-
-  final String image;
-  final Function press;
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.only(  
-          left: kDefaultPadding,
-          top: kDefaultPadding / 2,
-          bottom: kDefaultPadding / 2
-        ),
-        width: size.width * 0.8,
-        height: 185,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(  
-            fit: BoxFit.cover,
-            image: AssetImage(image)
-          )
-        ),
-      ),
-    );
-  }
-}
 
